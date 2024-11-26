72da449a0907a7ab774c87f8adf5409af5ae7242
prikshit100x <prikshit100x@gmail.com>
2024-11-26 20:48:47 +0530

    Add files via upload

---
+++ level1/let-const-var.js
@@ -0,0 +1,17 @@
+console.log("Prikshit");
+
+const accountId = 1443
+let accountEmail = "prikshit@gmail.com"
+
+var accountPassword ="123456"
+
+accountCity ="Shimla"
+
+// accountId =1232 // not allowed
+
+accountEmail="bawa@gmail.com"
+accountPassword ="231"
+accountCity="Kangra"
+console.log(accountId); // Error
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
